5233f487bbdfc15bdfc1426b9d5c9edd23a2fbf7
alialauren1 <157066441+alialauren1@users.noreply.github.com>
2025-02-23 01:22:16 -0800

    spinning (prelim)

--- src/practice_servo_test.py
+++ src/practice_servo_test.py
@@ -3,8 +3,10 @@ import pyb #dir(pyb)
 
 # FOR PB10, PWM 2/3 means 2 = timer number, 3 = channel
 
-timer = pyb.Timer(2, freq=10) #periodic freq of timer [Hz]
-ch2 = timer.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10, pulse_width=4)
+timer = pyb.Timer(2, freq=2) #periodic freq of timer [Hz]
+ch2 = timer.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10, pulse_width=150000)
+# PWM in nano seconds
+#100000,150000, 20000
   
 # SignalPin = PB_10
 # servo1 = pyb.Servo(SignalPin) #
