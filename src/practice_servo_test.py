7c8e7b287eb908b2f6de8af49ad9ed44246113e6
alialauren1 <157066441+alialauren1@users.noreply.github.com>
2025-02-23 21:00:47 -0800

    Update practice_servo_test.py

--- src/practice_servo_test.py
+++ src/practice_servo_test.py
@@ -1,12 +1,9 @@
 import pyb #dir(pyb)
-#import time
 
 # FOR PB10, PWM 2/3 means 2 = timer number, 3 = channel
 
-timer = pyb.Timer(2, freq=2) #periodic freq of timer [Hz]
-ch2 = timer.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10, pulse_width=150000)
-# PWM in nano seconds
-#100000,150000, 20000
+timer = pyb.Timer(2, freq=20) #periodic freq of timer [Hz]
+ch2 = timer.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10, pulse_width=100000)
+
   
-# SignalPin = PB_10
-# servo1 = pyb.Servo(SignalPin) #
+
