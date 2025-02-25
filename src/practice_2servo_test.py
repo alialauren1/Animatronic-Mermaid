98b9ba1989874e6fa9feddf2a6b8ea7ef0d41c28
alialauren1 <157066441+alialauren1@users.noreply.github.com>
2025-02-24 16:24:10 -0800

    updates

--- src/practice_2servo_test.py
+++ src/practice_2servo_test.py
@@ -2,8 +2,10 @@ import pyb #dir(pyb)
 
 # FOR PB10, PWM 2/3 means 2 = timer number, 3 = channel
 
-timer = pyb.Timer(2, freq=20) #periodic freq of timer [Hz]
-ch2 = timer.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10, pulse_width=100000) # PW in 10s of nano seconds I think
+timerA = pyb.Timer(2, freq=20) #periodic freq of timer [Hz]
+chA = timerA.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10, pulse_width=100000) # PW in 10s of nano seconds I think
 
-  
-
+servoA_min_theta = 0
+servoA_max_theta = 150
+servoB_min_theta = 0
+servoB_max_theta = 150
