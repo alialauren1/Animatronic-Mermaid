c302360c143ada30be6101909da9964cf9573f64
alialauren1 <157066441+alialauren1@users.noreply.github.com>
2025-03-04 19:03:47 -0800

    working servo 3 on test code

--- src/practice_Selected_servos_test.py
+++ src/practice_Selected_servos_test.py
@@ -14,10 +14,10 @@ import pyb #dir(pyb)
 # ch1 = timer1.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10, pulse_width=200000) # motor 1
 # ch2 = timer2.channel(2, pyb.Timer.PWM, pin=pyb.Pin.board.PB3, pulse_width=200000) # motor 2
 
-timer3 = pyb.Timer(8, freq=50)
-ch3 = timer3.channel(2, pyb.Timer.PWM, pin=pyb.Pin.board.PC7, pulse_width=100000)
-# 50000, 90000, 
-# 
+timer3 = pyb.Timer(2, freq=50) #periodic freq of timer [Hz]
+ch3 = timer3.channel(1, pyb.Timer.PWM, pin=pyb.Pin.board.PA5, pulse_width=40000)
+# 40000 (abt 180 deg), 50000, 90000 (abt 100 deg), 150000 (abt 40 deg), 190000 (abt -10 deg), 200000 (abt -25 deg) 
+
 # adc_S1 = pyb.ADC(pyb.Pin.board.PA6)# create analog object from a pin for servo 1
 # fdbck_S1 = adc_S1.read()
 # 
