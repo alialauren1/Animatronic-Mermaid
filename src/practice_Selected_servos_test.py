ae9ca9a8979e65285585639c472575d3096beed5
alialauren1 <157066441+alialauren1@users.noreply.github.com>
2025-03-07 11:06:15 -0800

    feedback initialized

--- src/practice_Selected_servos_test.py
+++ src/practice_Selected_servos_test.py
@@ -1,27 +1,22 @@
 import pyb #dir(pyb)
 
-# FOR PB10, PWM 2/3 means 2 = timer number, 3 = channel
-
-# timer = pyb.Timer(2, freq=50) #periodic freq of timer [Hz]
-# ch2 = timer.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10, pulse_width=50000) # PW in 10s of nano seconds I think
-# # PWM Tail Servos: 500-2500 microseconds, observed range is 50,000 to 200,000 with about 150 degree range
-# # PWM Hip Servo: Observed range is 40,000 to 200,000 with about 190 degree range
-
 
 # # setup of timer
-# timer1 = pyb.Timer(2, freq=50) #periodic freq of timer [Hz]
+timer1 = pyb.Timer(2, freq=50) #periodic freq of timer [Hz]
 # timer2 = pyb.Timer(2, freq=50) #periodic freq of timer [Hz]
-# ch1 = timer1.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10, pulse_width=200000) # motor 1
+
+# feedback
+adc_S1 = pyb.ADC(pyb.Pin.board.PA6)# create analog object from a pin for servo 1
+fdbck_S1 = adc_S1.read()
+print(fdbck_S1)
+
+ch1 = timer1.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10, pulse_width=50000) # motor 1
 # ch2 = timer2.channel(2, pyb.Timer.PWM, pin=pyb.Pin.board.PB3, pulse_width=200000) # motor 2
+# # PWM Tail Servos: 500-2500 microseconds, observed range is 50,000 to 200,000 with about 150 degree range
 
-timer3 = pyb.Timer(2, freq=50) #periodic freq of timer [Hz]
-ch3 = timer3.channel(1, pyb.Timer.PWM, pin=pyb.Pin.board.PA5, pulse_width=40000)
-# 40000 (abt 180 deg), 50000, 90000 (abt 100 deg), 150000 (abt 40 deg), 190000 (abt -10 deg), 200000 (abt -25 deg) 
+fdbck_S1 = adc_S1.read()
+print(fdbck_S1)
 
-# adc_S1 = pyb.ADC(pyb.Pin.board.PA6)# create analog object from a pin for servo 1
-# fdbck_S1 = adc_S1.read()
-# 
-# print(fdbck_S1)
-# at 200,000, ADC = 3364
-# at 50,000, ADC = 385
-# therefore, 
+# timer3 = pyb.Timer(2, freq=50) #periodic freq of timer [Hz]
+# ch3 = timer3.channel(1, pyb.Timer.PWM, pin=pyb.Pin.board.PA5, pulse_width=40000)
+# # 40000 (abt 180 deg), 50000, 90000 (abt 100 deg), 150000 (abt 40 deg), 190000 (abt -10 deg), 200000 (abt -25 deg) 
--- src/sin_wave_tail_control.py
+++ src/sin_wave_tail_control.py
@@ -10,14 +10,28 @@ timer3 = pyb.Timer(2, freq=50) #periodic freq of timer [Hz]
 # setup in Neutral Position: 1250 microseconds = 1250000ns
 neutral = 125000 # 10s of nanoseconds
 
-# setup of PWM channels to communicate with motor
-ch1 = timer1.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10, pulse_width=neutral) # hip in tail, joint 2
-ch2 = timer2.channel(2, pyb.Timer.PWM, pin=pyb.Pin.board.PB3, pulse_width=neutral) # knee in tail joint 3
-ch3 = timer3.channel(1, pyb.Timer.PWM, pin=pyb.Pin.board.PA5, pulse_width=neutral) # hip to body, joint 1
-#  
-# adc_S1 = pyb.ADC(pyb.Pin.board.PA6)# create analog object from a pin for servo 1
-# adc_S2 = pyb.ADC(pyb.Pin.board.PA7)# create analog object from a pin for servo 2
-    
+# setup of pins for (1) PWM channels to communicate with motor & (2) ADC to be read
+ch1 = timer1.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.PB10) # hip in tail, joint 2
+ch2 = timer2.channel(2, pyb.Timer.PWM, pin=pyb.Pin.board.PB3) # knee in tail joint 3
+ch3 = timer3.channel(1, pyb.Timer.PWM, pin=pyb.Pin.board.PA5) # hip to body, joint 1
+adc_S1 = pyb.ADC(pyb.Pin.board.PA6)# create analog object from a pin for servo 1, on joint 2
+adc_S2 = pyb.ADC(pyb.Pin.board.PA7)# create analog object from a pin for servo 2, on joint 3
+
+## Want to read current position before commanding neutral position
+fdbck_DC_S1 = adc_S1.read()
+fdbck_DC_S2 = adc_S2.read()
+# print(f"ADC_S1 = {fdbck_DC_S1:.2f}")
+
+# convert from ADC to PWM
+
+# spline will go here to move from whatever position read previously to then command neutral position which is shown below
+
+# Command neutral PWM position
+print('Commanding PW neutral position:')
+ch1.pulse_width(neutral)
+ch2.pulse_width(neutral)
+ch3.pulse_width(neutral)
+
 #setup of variables for Sin wave
 omega = math.radians(50) #deg/s to rad/s
 A_hip = 22; # keep in degrees
@@ -35,24 +49,22 @@ while True: # create loop that runs continuously until script is stopped
     theta_desired_knee = A_knee*math.sin(omega*current_t - phase_diff)
     
     # convert theta values to PWM values
-    pwm_desired_hip = k*(theta_desired_hip+75)
-    pwm_desired_knee = k*(theta_desired_knee+75)
+    pwm_desired_hip = (k*theta_desired_hip)+125000
+    pwm_desired_knee = (k*theta_desired_knee)+125000
+    pwm_desired_verticalax = (k*theta_desired_knee)+125000 #third servo that rotates at hip abt vertical axis
 
-    #fdbck_DC_S1 = adc_S1.read() #read feedback for hip servo
-    
-#     #Theory: P controller = (target - current position otherwise known as feedback pin)*gain
-#     PID_theta_err=(theta_knee - fdback_in_theta)*(1)
-#     PID_pwm_hip = k*((PID_theta_err+theta_knee)+75)
+    fdbck_DC_S1 = adc_S1.read() #read feedback for hip servo
+    fdbck_DC_S2 = adc_S2.read() #read feedback for knee servo
     
     # update values sent to motor through PWM channel
     ch1.pulse_width(round(pwm_desired_hip)) # round ensures integer going into PWM cmnd
     ch2.pulse_width(round(pwm_desired_knee))
-    ch3.pulse_width(round(pwm_desired_hip))
+    ch3.pulse_width(round(pwm_desired_verticalax))
     
     # uncomment to view value outputs
     print(f"Motor 1 Desired Angle: {theta_desired_hip:.2f}, Motor 2 Desired Angle: {theta_desired_knee:.2f}")
-#    print(f"Motor 1 Desired PWM: {pwm_desired_hip:.2f}")
-#    print(f"Motor 1 Measured PWM: {fdback_PWM_S1:.2f},ADC= {fdbck_DC_S1:.2f}")
+    print(f"Motor 1 Desired PWM: {pwm_desired_hip:.2f}")
+    print(f"ADC_S1= {fdbck_DC_S1:.2f}, ADC_S2= {fdbck_DC_S2:.2f}")
     print(current_t)
     
 
